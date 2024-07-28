d32210bc66dd1f5da342e5cd176f2800b10f08d6
Anish Mishra <anishkumarc92@gmail.com>
2024-07-28 12:21:44 +0530

    project complete (api issues remaining)

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,6 +8,7 @@ const Demo = () => {
         url: "",
         summary :""
     });
+    const [copied, setCopied] = useState('');
 
     const [allArticles, setAllArticles] = useState([]);
 
@@ -23,7 +24,7 @@ const Demo = () => {
 
     const handelSubmit = async(e) => {
         e.preventDefault();
-        const {data} = await getSummary({articleUrl: article.url});
+        const {data} = await getSummary({articleUrl: article.url});  
 
         if(data?.summary){
             const newArticle = {...article, summary: data.summary};
@@ -34,6 +35,12 @@ const Demo = () => {
             localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
         }
 
+    }
+
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(()=>setCopied(false), 3000);
     }
   return (
     <section className="mt-16 w-full max-w-xl ">
@@ -72,12 +79,12 @@ const Demo = () => {
                             onClick={()=>setArticle(item)}
                             className='link_card'
                         >
-                            <div className="copy_btn">
-                                <img src={copy} alt="copy" 
+                            <div className="copy_btn" onClick={()=>handleCopy(item.url)}>
+                                <img src={copied === item.url ? tick : copy} alt="copy_icon" 
                                     className="w-[40%] h-[40%] object-contain"
                                 />
                             </div>
-                            <p className="flex-1 font-satoshi text-blue-600 font-medium text-sm truncate">{item.url}</p>
+                            <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
                         </div>
                     ))}
             </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-            <img src={logo} alt="Summarise_logo" className='w-28 object-contain' />
+            <img src={logo} alt="Summarise_logo" className='w-20 object-contain' />
             <button type='button' onClick={()=> window.open("https://github.com/anish-k-mishra/Summarise-It")} className='black_btn '>
                 GitHub
             </button>
